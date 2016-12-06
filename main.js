f9116e3226086f540ce637c4e9f56f8aa44e355b
Thomas Harvey <tom@alush.co.uk>
2016-12-06 14:42:51 +0000

    whitespace and readability

--- main.js
+++ main.js
@@ -1,40 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, combineReducers } from 'redux'
+import { connect } from 'react-redux';
 import thunk from 'redux-thunk';
-import { connect, Provider } from 'react-redux';
-import { combineReducers } from 'redux';
-import { reducer as api, readEndpoint, setEndpointHost, setEndpointPath, setAccessToken } from 'redux-json-api';
+import { reducer as api, readEndpoint } from 'redux-json-api';
+import { setEndpointHost, setEndpointPath, setAccessToken } from 'redux-json-api';
 
-
-class TodoList extends React.Component {
-    componentWillMount() {
-        store.dispatch(setEndpointHost('http://localhost:3000'));
-        store.dispatch(setEndpointPath('/api/v1'));
-        store.dispatch(setAccessToken('1234'));
-
-        store.dispatch(readEndpoint('events'));
-    }
-
-    render() {
-        return (
-            <ul>
-            {this.props.events.data.map(event => (
-                <li key={event.id} >{event.id}</li>
-            ))}
-            </ul>
-            );
-    }
-
-};
-
-const mapStateToProps = ({ api: { events = { data: [] } } }) => ({ events });
 const reducer = combineReducers({
     api
 });
 
 const store = createStore(reducer, applyMiddleware(thunk))
+store.dispatch(setEndpointHost('http://localhost:3000'));
+store.dispatch(setEndpointPath('/api/v1'));
+store.dispatch(setAccessToken('1234'));
+
+class TodoList extends React.Component {
+    componentWillMount() {
+        store.dispatch(readEndpoint('events/1'));
+    }
+
+    render() {
+        return (
+            <ul>
+                {this.props.events.data.map(event => (
+                    <li key={event.id} >{event.id}</li>
+                ))}
+            </ul>
+        );
+    }
+
+};
+
+const mapStateToProps = (
+        { api: 
+            { events = { data: [] } }
+        }
+    ) => ({
+    events
+});
 
 const ApiResults = connect(
     mapStateToProps
