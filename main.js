dcc056c26b971f97f4394e0d224913118d58a8e3
Thomas Harvey <tom@alush.co.uk>
2016-12-06 23:39:11 +0000

    posts insted of todos

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class TodoList extends React.Component {
+class PostList extends React.Component {
     render() {
         return (<h1>Hello world</h1>);
     }
 }
 
-
 ReactDOM.render(
-    <TodoList />,
-    document.getElementById('todos')
+    <PostList />,
+    document.getElementById('posts')
 );
