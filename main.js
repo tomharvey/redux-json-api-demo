a7306fc4185480948bd9ca11f87df0a2de0eba8b
Thomas Harvey <tom@alush.co.uk>
2016-12-06 14:15:31 +0000

    get from endpoint and display results

--- main.js
+++ main.js
@@ -1,14 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class TodoList extends React.Component {
-    render() {
-        return (<h1>Hello world</h1>);
-    }
-}
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk';
+import { connect, Provider } from 'react-redux';
+import { combineReducers } from 'redux';
+import { reducer as api, readEndpoint, setEndpointHost, setEndpointPath, setAccessToken } from 'redux-json-api';
 
+
+class TodoList extends React.Component {
+    componentWillMount() {
+        store.dispatch(setEndpointHost('http://localhost:3000'));
+        store.dispatch(setEndpointPath('/api/v1'));
+        store.dispatch(setAccessToken('1234'));
+
+        store.dispatch(readEndpoint('events'));
+    }
+
+    render() {
+        return (
+            <ul>
+            {this.props.events.data.map(event => (
+                <li>{event.id}</li>
+            ))}
+            </ul>
+            );
+    }
+
+};
+
+const mapStateToProps = ({ api: { events = { data: [] } } }) => ({ events });
+const reducer = combineReducers({
+    api
+});
+
+const store = createStore(reducer, applyMiddleware(thunk))
+
+const ApiResults = connect(
+    mapStateToProps
+)(TodoList)
 
 ReactDOM.render(
-    <TodoList />,
+    <ApiResults store={store} />,
     document.getElementById('todos')
 );
