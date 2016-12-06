3beb65468753ad0b022ad0f1d8b39619e688a075
Thomas Harvey <tom@alush.co.uk>
2016-12-06 15:05:18 +0000

    naming for todo objects

--- main.js
+++ main.js
@@ -18,14 +18,14 @@ store.dispatch(setAccessToken('1234'));
 
 class TodoList extends React.Component {
     componentWillMount() {
-        store.dispatch(readEndpoint('events/1'));
+        store.dispatch(readEndpoint('todos'));
     }
 
     render() {
         return (
             <ul>
-                {this.props.events.data.map(event => (
-                    <li key={event.id} >{event.id}</li>
+                {this.props.todos.data.map(todo => (
+                    <li key={todo.id} >{todo.description}</li>
                 ))}
             </ul>
         );
@@ -35,8 +35,8 @@ class TodoList extends React.Component {
 
 const mapStateToProps = (state) => {
     console.log(state)
-    const events = state.api.events || { data: [] };
-    return {events}
+    const todos = state.api.todos || { data: [] };
+    return {todos}
 };
 
 const ApiResults = connect(
